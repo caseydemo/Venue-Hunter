eb73326b9dc8b7e7ca540cffa06d7d5255d1d316
caseydemo <cdemattina@gmail.com>
2017-11-24 22:24:27 -0500

    found a nice style to go with

--- resources/views/places/input.blade.php
+++ resources/views/places/input.blade.php
@@ -7,34 +7,37 @@ Let's Find a Venue
 @section('content')
 <body>
 
-<h1>Club Hunter!</h1>
+     
 
-<form action="/" method="post">
-        <input style="text-align:center" type="hidden" name="_token" value="{{ csrf_token() }}">
-        <ul><strong>City: <input type="text" name="city"><br></strong></ul>
-        <ul><strong>State: <input type="text" name="state"><br></strong></ul>
-        <ul><strong>Keyword: <input type="text" name="keyword"><br></strong></ul>
-        <input type="submit">
-    </form>
+	<h1>Club Hunter!</h1>
 
 
-    @if(!empty($recent_city))
-    <h3>Recent Searches</h3>
-	<table class="table table-striped">
-	  <tr>
-	    <th>City</th>
-	    <th>Keyword</th> 
-	    <th>Searched At</th>
-	  </tr>
-	  @for($i=0; $i<$search_count-1; $i++)
-	  	<tr>
-		    <td>{{ $recent_city[$i] }}</td>
-		    <td>{{ $recent_keyword[$i] }}</td>
-		    <td>{{ $recent_search_timestamp[$i] }}</td>
-		</tr>
-		@endfor
-	</table>
+	<form action="/" method="post">
+	        <input style="text-align:center" type="hidden" name="_token" value="{{ csrf_token() }}">
+	        <ul><strong>City: <input type="text" name="city"><br></strong></ul>
+	        <ul><strong>State: <input type="text" name="state"><br></strong></ul>
+	        <ul><strong>Keyword: <input type="text" name="keyword"><br></strong></ul>
+	        <input type="submit">
+	    </form>      
 
-    @endif
+
+	    @if(!empty($recent_city))
+	    <h3>Recent Searches</h3>
+		<table class="table table-striped">
+		  <tr>
+		    <th>City</th>
+		    <th>Keyword</th> 
+		    <th>Searched At</th>
+		  </tr>
+		  @for($i=0; $i<$search_count-1; $i++)
+		  	<tr>
+			    <td>{{ $recent_city[$i] }}</td>
+			    <td>{{ $recent_keyword[$i] }}</td>
+			    <td>{{ $recent_search_timestamp[$i] }}</td>
+			</tr>
+			@endfor
+		</table>
+
+	    @endif
 </body>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,25 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+                    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
-        <title>Laravel</title>
+            <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+              <script src="https://use.fontawesome.com/14f1f2c704.js"></script>
+
+
+
+        <title>Venue Hunter</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #1F2124;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -67,29 +75,27 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div>
                     @if (Auth::check())
-                        <a href="{{ url('/places/input') }}">Home</a>
-                    @else
+                        <a href="{{ url('/places/input') }}"></a>
+                        <div class="jumbotron vertical-center">
+                            <div class="inner-jumbo">
+                              <h1 class="display-3">VENUE HUNTER</h1>
+                              <p class="lead">Give us a city. We'll give you numbers to start calling.</p>
+                              <p style="text-align:center" class="play">
+                                <a href="{{ url('/places/input') }}" role="button">
+                                <i style="color:#383A3F" class="fa fa-play" aria-hidden="true"></i>
+                                Let's Go</a>
+                              </p>
+                            </div>
+                        </div>
+                         @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            
         </div>
     </body>
 </html>
