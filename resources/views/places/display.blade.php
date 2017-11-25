567e477229bb841da9bfaf64b0b003b94247054f
caseydemo <cdemattina@gmail.com>
2017-11-25 16:34:17 -0500

    formatted nearby search to new styling

--- resources/views/places/display.blade.php
+++ resources/views/places/display.blade.php
@@ -16,21 +16,30 @@ Let's Find a Venue
          <button type="submit" name="button" value="save" class="btn btn-md btn-default">Save Search</button>
     </form>
 
+
     <body>
+        <div class="flex-center position-ref full-height">
+            <div class="jumbotron vertical-center">
+                <h1 class="display-3">VENUE HUNTER</h1>
+                <p class="lead">Give us a city. We'll give you numbers to start calling.</p>
 
-        <h4>Search Results for {{ $cityName }}</h4>
-        <h4>{{ $searchDate }}</h4>
-
-        	@for($i=0; $i<$loopCount-1; $i++)
-        	<hr>
-        		<ul><h1>Name: {{ $nameArray[$i] }}  </h1></ul>
-                <ul><strong>Address: {{ $vicinityArray[$i] }}</strong></ul>
-                <ul><strong>Open Now: {{ $open_now_array[$i] }}</strong></ul>        
-                <ul><strong><a href="/detail/{{$place_id_array[$i]}}">Details</a></strong></ul>
-                <hr>
-        	@endfor
+            </div>
+        </div>
 
 
+        
+            <div class="jumbotron vertical-center">
+                    <h2>Search Results for {{ $cityName }}</h2>
+                    <h2>{{ $searchDate }}</h2>
+                    @for($i=0; $i<$loopCount-1; $i++)
+                    <hr>
+                        <ul><h3>Name: {{ $nameArray[$i] }}  </h3></ul>
+                        <ul><strong>Address: {{ $vicinityArray[$i] }}</strong></ul>
+                        <ul><strong>Open Now: {{ $open_now_array[$i] }}</strong></ul>        
+                        <ul><strong><a href="/detail/{{$place_id_array[$i]}}">Details</a></strong></ul>
+                        <hr>
+                    @endfor
+            </div>
     </body>
 @endsection
 
