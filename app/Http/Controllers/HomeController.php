44b07aecf5077c18fbab750b9095fa9a5793dd77
caseydemo <cdemattina@gmail.com>
2017-11-27 10:22:45 -0500

    try this

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
     public function index()
     {
         $recent_searches = \App\Search::get();
+        dd($recent_searches);
         $search_count = count($recent_searches)+1;
 
 
