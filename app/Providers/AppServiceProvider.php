ec6d489ea6b5290886db81dbb575de0cfb82d150
caseydemo <cdemattina@gmail.com>
2017-11-27 08:52:22 -0500

    took out the if, now just forcing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        If(env('APP_ENV') !== 'local') { $url->forceSchema('https'); }
-    }
+        $url->forceSchema('https');    }
 
     /**
      * Register any application services.
