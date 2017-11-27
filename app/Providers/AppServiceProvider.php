babbf16a2f2d05d32b984aaeaca5f0e9a4411f90
caseydemo <cdemattina@gmail.com>
2017-11-27 08:50:15 -0500

    added if statement to force https schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        If(env('APP_ENV') !== 'local') { $url->forceSchema('https'); }
     }
 
     /**
