aee8cfaef3679e5ab00469a138568ca31f78a7b4
caseydemo <cdemattina@gmail.com>
2017-12-05 11:46:36 -0500

    streamlined the details page and put it all in modals, yo

--- resources/views/places/detail.blade.php
+++ resources/views/places/detail.blade.php
@@ -8,6 +8,79 @@ Let's Find a Venue
 <body>
 
 
+
+
+<!-- reviewsModal -->
+<div class="modal fade" id="reviewsModal" tabindex="-1" role="dialog" aria-labelledby="reviewsModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="reviewsModalLabel">Reviews</h4>
+      </div>
+      <div class="modal-body">
+        @if($review_bool)
+            <h2>Reviews:</h2>
+            <hr>
+            @for($i=0; $i<3; $i++)
+                <h3>{{$i+1}}. By:{{ $author_name_array[$i] }}, {{ $time_description_array[$i]}}</h3>
+                <div class="review-body">
+                    @for($j=0; $j<$rating_array[$i]; $j++)
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                    @endfor
+                    <p>{{$rating_array[$i]}} Stars</p>
+                    <p>{{ $review_text_array[$i] }}</p>
+               
+
+                </div>
+                <hr>
+                <br>
+            @endfor
+            </strong></ul>
+            
+        @endif
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- hoursModal -->
+<div class="modal fade" id="hoursModal" tabindex="-1" role="dialog" aria-labelledby="hoursModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="hoursModalLabel">Opening Hours</h4>
+      </div>
+      <div class="modal-body">
+         <strong><h2>Open Now:
+        @if($open_now) 
+            Yes <i class="fa fa-clock-o" aria-hidden="true"></i></h2></strong>
+        @else
+            No</h2>
+        @endif
+        
+        @if($hours[0]!='N/A')
+            <ul><strong>Hours:</ul></strong> 
+            @for($i=0; $i<7; $i++)
+                <p>{{ $hours[$i] }}</p>
+            @endfor
+        @endif
+        </strong>      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     <form class="form-horizontal" method="post" action="/contacts" id="detail">
         <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
         <input type="hidden" name="business_name" value="{{ $name }}"> 
@@ -19,12 +92,23 @@ Let's Find a Venue
          <input type="hidden" name="longitude" value="{{ $longitude }}">
             
     </form>
+<div class="outer-well">
+    <div class="top-left-button">
+        @if($website!= '#')
+            <a href="{{ $website }}" target="_blank"><button class="round-button btn-lg hvr-pulse">Visit their Site</button></a>
+        @else
+            <a href="#" target="_blank"><button class="round-button btn-lg hvr-pulse">Visit their Site</button></a>
+         @endif
+    </div>
+        <div class="top-right-button">
+            <button type="submit" name="button" value="save" form="detail" class="round-button btn-lg hvr-pulse">Add to Contacts</button>
+        </div>
     <div class="well detail-title-well">
         <h1 class="detail-title">{{ $name }}</h1>
         <p class="lead">{{ $address }}</p>
         <p class="lead"><a href="tel:'{{ $phone }}'">{{ $phone }}</a> </p>
-       
     </div>
+</div>
 
      
 
@@ -34,19 +118,24 @@ Let's Find a Venue
             <iframe class="i-frame" src="{{$url}}">
             </iframe>
                     
-            <div class="web-div">
-                @if($website!= '#')
-                    
-                    <a href="{{ $website }}" target="_blank"><button class="btn-lg hvr-pulse">{{$name}}'s Website</button></a>
-                @endif
+            <div class="bottom-left-button">
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="round-button btn-lg hvr-pulse" data-toggle="modal" data-target="#hoursModal">
+                      Hours
+                    </button>
+                
             </div>
-            <div class="contact-div">
-                <button type="submit" name="button" value="save" form="detail" class="btn-lg hvr-pulse">Add to Contacts</button>
+            <div class="bottom-right-button">
+                <!-- Button trigger modal -->
+                <button type="button" class="round-button btn-lg hvr-pulse" data-toggle="modal" data-target="#reviewsModal">
+                  Reviews
+                </button>
             </div>
         </div>
         
 
-<div class="flex-center position-ref full-height">
+<!-- <div class="flex-center position-ref full-height">
             <div class="jumbotron vertical-center">
         
         <strong><h2>Open Now:
@@ -87,7 +176,7 @@ Let's Find a Venue
         </div>
     </div>
 
-		
+		 -->
 	
 </body>
 @endsection
